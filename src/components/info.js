25fd560e0b9c35c3cf4b963ab097758aa0ccc17f
Andrew Leonardi <ndrewleonardi2@users.noreply.github.com>
2019-05-02 17:41:31 -0700

    added preliminary info layout

--- src/components/info.js
+++ src/components/info.js
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
+import { Card, Button, Checkbox, Form } from 'semantic-ui-react'
 import '../styles/Info.css';
 
 class Info extends Component {
   render() {
     return (
-      <div className="Cart">
-        <p>The Info</p>
-      </div>
+      <Card className="Info">
+        <Form className="Personal">
+          <Form.Field>
+            <label>Name on Card</label>
+            <input placeholder='Name On Card' />
+          </Form.Field>
+          <Form.Field>
+            <label>Credit Card Number</label>
+            <input placeholder='Credit Card Number' />
+          </Form.Field>
+          <Form.Field>
+            <label>Expiration Date</label>
+            <input placeholder='Expiration Date' />
+          </Form.Field>
+          <Form.Field>
+            <label>CVC</label>
+            <input placeholder='CVC' />
+          </Form.Field>
+        </Form>
+        <Form className="Shipping">
+          <Form.Field>
+            <label>Name</label>
+            <input placeholder='Name' />
+          </Form.Field>
+          <Form.Field>
+            <label>Address</label>
+            <input placeholder='Address' />
+          </Form.Field>
+          <Form.Field>
+            <label>Address 2</label>
+            <input placeholder='Address 2' />
+          </Form.Field>
+          <Form.Field>
+            <label>City</label>
+            <input placeholder='City' />
+          </Form.Field>
+          <Form.Field>
+            <label>Country</label>
+            <input placeholder='Country' />
+          </Form.Field>
+          <Form.Field>
+            <label>State</label>
+            <input placeholder='State' />
+          </Form.Field>
+          <Form.Field>
+            <label>Zip Code</label>
+            <input placeholder='Zip Code' />
+          </Form.Field>
+          <Checkbox label='Billing address same as shipping' />
+        </Form>
+        <Button type='submit'>Place Order</Button>
+      </Card>
+
     );
   }
 }
